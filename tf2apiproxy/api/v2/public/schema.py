bd2bfb640576efbe2b95c6adbeda3bdd07d76bc0
Troy Melhase <troy@gci.net>
2011-03-10 16:56:14 -0900

    make dist.

--- tf2apiproxy/api/v2/public/schema.py
+++ tf2apiproxy/api/v2/public/schema.py
@@ -12,26 +12,26 @@ class Schema(ApiHandler):
                      '?key=%s&format=json&language=%s')
 
     img_fixes = {
-	5027 : 'media/img/TF_Tool_PaintCan_1.png',
-	5028 : 'media/img/TF_Tool_PaintCan_2.png',
-	5029 : 'media/img/TF_Tool_PaintCan_3.png',
-	5030 : 'media/img/TF_Tool_PaintCan_4.png',
-	5031 : 'media/img/TF_Tool_PaintCan_5.png',
-	5032 : 'media/img/TF_Tool_PaintCan_6.png',
-	5033 : 'media/img/TF_Tool_PaintCan_7.png',
-	5034 : 'media/img/TF_Tool_PaintCan_8.png',
-	5035 : 'media/img/TF_Tool_PaintCan_9.png',
-	5036 : 'media/img/TF_Tool_PaintCan_10.png',
-	5037 : 'media/img/TF_Tool_PaintCan_11.png',
-	5038 : 'media/img/TF_Tool_PaintCan_12.png',
-	5039 : 'media/img/TF_Tool_PaintCan_13.png',
-	5040 : 'media/img/TF_Tool_PaintCan_14.png',
-	5051 : 'media/img/TF_Tool_PaintCan_15.png',
-	5052 : 'media/img/TF_Tool_PaintCan_16.png',
-	5053 : 'media/img/TF_Tool_PaintCan_17.png',
-	5054 : 'media/img/TF_Tool_PaintCan_18.png',
-	5055 : 'media/img/TF_Tool_PaintCan_19.png',
-	5056 : 'media/img/TF_Tool_PaintCan_20.png',
+	5027 : 'media/img/TF_Tool_PaintCan_1.png',      # Indubitably Green
+	5028 : 'media/img/TF_Tool_PaintCan_2.png',      # Zephaniah's Greed
+	5029 : 'media/img/TF_Tool_PaintCan_3.png',      # Noble Hatter's Violet
+	5030 : 'media/img/TF_Tool_PaintCan_4.png',      # Color No. 216-190-216
+	5031 : 'media/img/TF_Tool_PaintCan_5.png',      # A Deep Commitment to Purple
+	5032 : 'media/img/TF_Tool_PaintCan_6.png',      # Mann Co. Orange
+	5033 : 'media/img/TF_Tool_PaintCan_7.png',      # Muskelmannbraun
+	5034 : 'media/img/TF_Tool_PaintCan_8.png',      # Peculiarly Drab Tincture
+	5035 : 'media/img/TF_Tool_PaintCan_9.png',      # Radigan Conagher Brown
+	5036 : 'media/img/TF_Tool_PaintCan_10.png',     # Ye Olde Rustic Colour
+	5037 : 'media/img/TF_Tool_PaintCan_11.png',     # Australium Gold
+	5038 : 'media/img/TF_Tool_PaintCan_12.png',     # Aged Moustache Grey
+	5039 : 'media/img/TF_Tool_PaintCan_13.png',     # An Extraordinary Abundance of Tinge
+	5040 : 'media/img/TF_Tool_PaintCan_14.png',     # A Distinctive Lack of Hue
+	5051 : 'media/img/TF_Tool_PaintCan_15.png',     # Pink as Hell
+	5052 : 'media/img/TF_Tool_PaintCan_16.png',     # A Color Similar to Slate
+	5053 : 'media/img/TF_Tool_PaintCan_17.png',     # Drably Olive
+	5054 : 'media/img/TF_Tool_PaintCan_18.png',     # The Bitter Taste of Defeat and Lime
+	5055 : 'media/img/TF_Tool_PaintCan_19.png',     # The Color of a Gentlemann's Business Pants
+	5056 : 'media/img/TF_Tool_PaintCan_20.png',     # Dark Salmon Injustice
     }
 
     @property
